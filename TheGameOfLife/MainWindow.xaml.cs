7643e41adf9be424edf80bcde7dad8454c69821e
carloshermoso64 <carloshermoso29@hotmail.com>
2020-09-30 18:41:11 +0200

    Design of the Main Windows

--- TheGameOfLife/MainWindow.xaml.cs
+++ TheGameOfLife/MainWindow.xaml.cs
@@ -27,16 +27,16 @@ namespace TheGameOfLife
         public MainWindow()
         {
             InitializeComponent();
-            
-
-
-
 
             DispatcherTimer timer = new DispatcherTimer();
-            Dispatcher.BeginInvoke(DispatcherPriority.Loaded, new Action(() => GridSetup()));
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += timer_Tick;
             timer.Start();
+
+
+
+
+
         }
 
         void timer_Tick(object sender, EventArgs e)
@@ -44,68 +44,7 @@ namespace TheGameOfLife
             lblTime.Content = DateTime.Now.ToLongTimeString();
         }
 
-        private void GridSetup()
-        {
-            int columns = 41;
-
-            int r = 0;
-            int c = 0;
-
-            foreach (Button cell in FindVisualChildren<Button>(this).Where(b => String.IsNullOrEmpty(b.Name)))
-            {
-                cell.Name = string.Format("Cell_{0}_{1}", r, c);
-                cell.Click += Cell_Click;
-                Map.Add(new Cell(cell)
-                {
-                    Name = cell.Name,
-                    coorX = c,
-                    coorY = r
-                });
-                if (++c > columns)
-                {
-                    c = 0;
-                    ++r;
-                }
-            }
-        }
-
-
-        private void Cell_Click(object sender, RoutedEventArgs e)
-        {
-            Button button = sender as Button;
-            Cell cell = Cell.Grid.Single(c => c.Name == button.Name);
-            if (!cell.alive)
-            {
-                cell.TurnOn();
-            }
-            else
-            {
-                cell.TurnOff();
-            }
-        }
-
-
-
-        //Helper function to find components
-        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
-        {
-            if (depObj != null)
-            {
-                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
-                {
-                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
-                    if (child != null && child is T)
-                    {
-                        yield return (T)child;
-                    }
-
-                    foreach (T childOfChild in FindVisualChildren<T>(child))
-                    {
-                        yield return childOfChild;
-                    }
-                }
-            }
-        }
+  
 
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
